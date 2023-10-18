295dc0e13c16297748b16b13d4c4df6b8b67d9bf
Hepheir <hepheir@gmail.com>
2023-10-18 22:47:05 +0900

    Update MainUsingSelectToppingPizza to use Decorator pattern

--- src/MainUsingSelectToppingPizza.java
+++ src/MainUsingSelectToppingPizza.java
@@ -1,14 +1,23 @@
+import java.lang.reflect.Constructor;
 import java.util.List;
 
 class MainUsingSelectToppingPizza {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         List<String> list;
         LoadToppings loadToppings = new LoadToppings("toppings.txt");
         list = loadToppings.load();
     
-        SelectToppingPizza pizza = new SelectToppingPizza(15);
+        Pizza pizza = new Pizza(15);
         for (String topping : list) {
-            pizza.addTopping(topping);
+            try {
+                Class<?> decoratorClass = Class.forName(topping);
+                Constructor<PizzaDecorator> constructor = (Constructor<PizzaDecorator>) decoratorClass.getConstructor(Pizza.class);
+                pizza = constructor.newInstance(pizza);
+            }
+            catch (Exception e) {
+                System.out.println(topping + " 토핑은 존재하지 않습니다.");
+                return;
+            }
         }
         System.out.printf("피자: %s, 크기: %d, 가격: %d\n", pizza.getName(), pizza.getSize(), pizza.getPrice());
     }
